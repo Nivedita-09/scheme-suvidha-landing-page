d8cc768f1b254c3e47527b2b308bbd7a8908f7bd
Weather forecast with Alan AI <niveditamahato2001@gmail.com>
2023-04-06 22:47:28 +0530

    Code till now

--- components/camps/addCamps/AddCamps.js
+++ components/camps/addCamps/AddCamps.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 import style from "./AddCamps.module.scss";
-import { useForm } from "react-hook-form";
+import { set, useForm } from "react-hook-form";
 import DatePicker from "react-datepicker";
-
 import "react-datepicker/dist/react-datepicker.css";
+import { BsFillCloudArrowUpFill } from "react-icons/bs";
+import { uploadCampData } from "@/api/uploadCampData";
 
 const campcategory = [
   "All",
@@ -13,17 +14,47 @@ const campcategory = [
 ];
 
 export default function AddCamps() {
-  const [select, setSelect] = useState(campcategory[0]);
+  // const [select, setSelect] = useState(campcategory[0]);
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
-  const handleChange = (e) => {
-    setSelect(e.target.value);
-  };
+  const [campTemplate, setCampTemplate] = useState("");
+  const [campDetails, setCampDetails] = useState({
+    CategoryCamp: campcategory[0],
+    OrganizationCamp: "",
+    CampBenefit: "",
+    NameCamp: "",
+    StartDate: "",
+    EndDate: "",
+    CampDiscription: "",
+    CampImage: "",
+    CampPlace: "",
+    CampEmail: "",
+    CampContact: "",
+    CampFrom: "",
+    CampTill: "",
+  });
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm();
 
+  function pick_campTemplate(event) {
+    setCampTemplate(Event.target.files[0]);
+  }
+  function pick_startDate(event) {
+    setStartDate(Event.target.value);
+  }
+  const submit = async () => {
+    await uploadCampData({
+      campDetails: campDetails,
+      campTemplate: campTemplate,
+    });
+  };
   return (
     <>
       <div className={style.AddCamps}>
-        <form className={style.camps_form_div}>
+        <form className={style.camps_form_div} onSubmit={handleSubmit(submit)}>
           <div className={style.CampTitle}>Campaign Details</div>
           <div className={style.camps_form_details}>
             <div className={style.camps_left_section}>
@@ -32,6 +63,10 @@ export default function AddCamps() {
                 <input
                   type="text"
                   placeholder="Campaign Name"
+                  value={campDetails["NameCamp"]}
+                  onChange={(e) =>
+                    setCampDetails({ ...campDetails, NameCamp: e.target.value })
+                  }
                   className={style.camp_input}
                 />
               </div>
@@ -39,6 +74,13 @@ export default function AddCamps() {
                 <label className={style.camp_label}>Organization Name</label>
                 <input
                   type="text"
+                  value={campDetails["OrganizationCamp"]}
+                  onChange={(e) =>
+                    setCampDetails({
+                      ...campDetails,
+                      OrganizationCamp: e.target.value,
+                    })
+                  }
                   placeholder="Minister of Social Justice and Empowerment"
                   className={style.camp_input}
                 />
@@ -47,20 +89,28 @@ export default function AddCamps() {
                 <label className={style.camp_label}>Campaign Location</label>
                 <input
                   type="text"
+                  value={campDetails["CampPlace"]}
+                  onChange={(e) =>
+                    setCampDetails({
+                      ...campDetails,
+                      CampPlace: e.target.value,
+                    })
+                  }
                   placeholder="Address of your ngo........"
                   className={style.camp_input}
                 />
               </div>
               <div className={style.camp_content}>
                 <label className={style.camp_label}>Category</label>
-                {/* <input
-                  type="text"
-                  placeholder="Address of your ngo........"
-                  className={style.camp_input}
-                /> */}
+
                 <select
-                  value={select}
-                  onChange={handleChange}
+                  value={campDetails["CategoryCamp"]}
+                  onChange={(e) =>
+                    setCampDetails({
+                      ...campDetails,
+                      CategoryCamp: e.target.value,
+                    })
+                  }
                   className={style.camp_input}
                 >
                   {campcategory.map((value, index) => (
@@ -76,23 +126,46 @@ export default function AddCamps() {
                   <div className={style.datepicker}>
                     <label>Start Date</label>
                     <DatePicker
-                      selected={startDate}
-                      onChange={(date) => setStartDate(date)}
+                      selected={campDetails["StartDate"]}
+                      // onChange={(e) =>
+                      //   setStartDate({
+                      //     ...campDetails,
+                      //     StartDate: e.target.value,
+                      //   })
+                      // }
+                      onChange={pick_startDate}
+                      dateFormat="dd/MM/yyyy"
                       className={style.camp_input_date}
                     />
+                    {/* <input
+                      type="date"
+                      value={campDetails["StartDate"]}
+                      onChange={(e) =>
+                        setCampDetails({
+                          ...campDetails,
+                          StartDate: e.target.value,
+                        })
+                      }
+                      className={style.camp_input_date}
+                    /> */}
                   </div>
                   <div className={style.datepicker}>
                     <label>End Date</label>
-                    <DatePicker
-                      selected={endDate}
-                      onChange={(date) => setEndDate(date)}
+
+                    <input
+                      type="date"
+                      format="dd/mm/yyyy"
+                      value={campDetails["EndDate"]}
+                      onChange={(e) =>
+                        setCampDetails({
+                          ...campDetails,
+                          EndDate: e.target.value,
+                        })
+                      }
                       className={style.camp_input_date}
                     />
                   </div>
                 </div>
-              </div>
-              <div className={style.camp_content}>
-                <label className={style.camp_label}>Template Photo</label>
               </div>
             </div>
             <div className={style.camps_right_section}>
@@ -100,6 +173,13 @@ export default function AddCamps() {
                 <label className={style.camp_label}>Campaign Details</label>
                 <textarea
                   type="text"
+                  value={campDetails["CampDiscription"]}
+                  onChange={(e) =>
+                    setCampDetails({
+                      ...campDetails,
+                      CampDiscription: e.target.value,
+                    })
+                  }
                   placeholder="NGO details like what your ngo help to other people, etc"
                   className={style.camp_textarea}
                 />
@@ -110,6 +190,13 @@ export default function AddCamps() {
                 </label>
                 <input
                   type="text"
+                  value={campDetails["CampEmail"]}
+                  onChange={(e) =>
+                    setCampDetails({
+                      ...campDetails,
+                      CampEmail: e.target.value,
+                    })
+                  }
                   placeholder="Enter ngo email  id "
                   className={style.camp_input}
                 />
@@ -120,22 +207,71 @@ export default function AddCamps() {
                 </label>
                 <input
                   type="text"
+                  value={campDetails["CampContact"]}
+                  onChange={(e) =>
+                    setCampDetails({
+                      ...campDetails,
+                      CampContact: e.target.value,
+                    })
+                  }
                   placeholder="Enter Ngo contact No."
                   className={style.camp_input}
                 />
               </div>
+
               <div className={style.camp_content}>
                 <label className={style.camp_label}>Time</label>
-                <input
-                  type="text"
-                  placeholder="Campaign Name"
-                  className={style.camp_input}
-                />
+                <div className={style.datepicker_div}>
+                  <div className={style.datepicker}>
+                    <label>From</label>
+                    <input
+                      type="time"
+                      value={campDetails["CampFrom"]}
+                      onChange={(e) =>
+                        setCampDetails({
+                          ...campDetails,
+                          CampFrom: e.target.value,
+                        })
+                      }
+                      className={style.camp_input_date}
+                    />
+                  </div>
+                  <div className={style.datepicker}>
+                    <label>Till</label>
+                    <input
+                      type="time"
+                      value={campDetails["CampTill"]}
+                      onChange={(e) =>
+                        setCampDetails({
+                          ...campDetails,
+                          CampTill: e.target.value,
+                        })
+                      }
+                      className={style.camp_input_date}
+                    />
+                  </div>
+                </div>
               </div>
             </div>
           </div>
-          <div>
-            <input type="submit" />
+          <div className={style.camp_content_template}>
+            <div className={style.uploadCampDiv}>
+              <label className={style.camp_label}>Template Photo</label>
+            </div>
+            <div className={style.uploadDiv}>
+              <input
+                type="file"
+                id="campTemplate"
+                onChange={pick_campTemplate}
+                className={style.uploadInput}
+              />
+              <label htmlFor="campTemplate" className={style.upload}>
+                <BsFillCloudArrowUpFill />
+              </label>
+            </div>
+          </div>
+          <div className={style.submit_Div}>
+            <input type="submit" onClick={submit} className={style.SubmitBtn} />
           </div>
         </form>
       </div>
